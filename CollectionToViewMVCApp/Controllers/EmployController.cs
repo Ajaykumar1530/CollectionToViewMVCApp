45a4f8e74e7860fbec4372d3fc35baf4da21f4ed
HP <rajashekar007009@gmail.com>
2023-02-03 14:15:22 +0530

    Add project files.

---
+++ CollectionToViewMVCApp/Controllers/EmployController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CollectionToViewMVCApp.Models;
+
+namespace CollectionToViewMVCApp.Controllers
+{
+    public class EmployController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Creating Objects for Employ Model Class
+            Employ emp1 = new Employ();
+            emp1.EmpId = 101;
+            emp1.EName = "Rajashekar";
+            emp1.Salary = 50213;
+
+            Employ emp2 = new Employ();
+            emp2.EmpId = 102;
+            emp2.EName = "Maniii";
+            emp2.Salary = 61798;
+
+            Employ emp3 = new Employ();
+            emp3.EmpId = 103;
+            emp3.EName = "Ajay";
+            emp3.Salary = 65000;
+
+         /*   List<Employ> employees = new List<Employ>()
+            {
+                new Employ{EmpId=101,EName="Assaaam",Salary=2565 }
+            };*/
+
+            //Passing Objects to Collection
+
+            List<Employ> employees = new List<Employ>();
+            employees.Add(emp1);
+            employees.Add(emp2);
+            employees.Add(emp3);
+
+            return View(employees);
+        }
+    }
+}
---
+++ CollectionToViewMVCApp/Models/Employ.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollectionToViewMVCApp.Models
+{
+    public class Employ
+    {
+        public int EmpId { get; set; }
+        public string EName { get; set; }
+        public double Salary { get; set; }
+    }
+}
